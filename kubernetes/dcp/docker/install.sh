f61d4ee07d608a7872ac3bfe6ef230cefc4cff45
Brian Merrick <brianmerrick@boomi.com>
2022-03-04 17:02:33 -0700

    closer for DCP

--- kubernetes/dcp/docker/install.sh
+++ kubernetes/dcp/docker/install.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 # Copyright (c) 2015 Boomi, Inc.
 
+echo "unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz"
 wget -q https://storage.googleapis.com/unifi-hd-4tb/unifi-release/${DCP_VERSION}/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz -P /tmp
 
 sudo tar xvf /tmp/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz -C /tmp > /dev/null 2>&1
 
 sudo yum -y install java-1.8.0-openjdk-devel
 
+sudo tar xvf /tmp/adapters_no_internet_${DCP_DOT_VERSION}.tar.gz -C /usr/local/unifing-${DCP_VERSION}/opt > /dev/null 2>&1
+
 sudo /tmp/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x/install_unifi_prereqs_catalog.sh --unifi-services-user unifi --unifi-services-group unifi
+
+sudo yum -y downgrade java-1.8.0-openjdk-headless-1.8.0.282.b08 \
+  java-1.8.0-openjdk-devel-1.8.0.282.b08 \
+  java-1.8.0-openjdk-1.8.0.282.b08
 
 shopt -s expand_aliases
 source /home/unifi/.bashrc
 
+echo "unifing-catalog-${DCP_DOT_VERSION}.tar.gz"
 wget -q https://storage.googleapis.com/unifi-hd-4tb/unifi-release/${DCP_VERSION}/unifing-catalog-${DCP_DOT_VERSION}.tar.gz -P /tmp
 sudo tar xvf /tmp/unifing-catalog-${DCP_DOT_VERSION}.tar.gz -C /usr/local > /dev/null 2>&1
 
@@ -26,7 +34,17 @@ echo "UNIFI_VIRT_ENV=/usr/local/unifi_virtualenv" >> /usr/local/unifi/unifi_env.
 pg_start
 pg_status
 cassandra_start
-cassandra_status
+
+ICASSANDRA_TIMEOUT=30
+i="0"
+
+while cassandra_status; [ $? -ne 0 ]; do
+  sleep 1; i=$[$i+1]
+  if [[ $i -ge $ICASSANDRA_TIMEOUT ]]; then
+    echo "Cassandra not started. Exiting ..."
+	break
+  fi
+done
 
 pip install /usr/local/unifi/unifi_pylib/django_celery_beat-1.4.0.Unifi-py2.py3-none-any.whl
 
