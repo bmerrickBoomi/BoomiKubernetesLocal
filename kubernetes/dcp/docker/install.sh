fb6b8e9010bdbc720072d25917fce90ed3b27ff3
Brian Merrick <brianmerrick@boomi.com>
2022-03-07 10:57:09 -0700

    DCP install fix

--- kubernetes/dcp/docker/install.sh
+++ kubernetes/dcp/docker/install.sh
@@ -1,75 +1,63 @@
 #!/bin/bash
 # Copyright (c) 2015 Boomi, Inc.
 
-time (
-  IS_EMPTY=`$(ls -A /usr/local/unifing-catalog-${DCP_DOT_VERSION})`
+IS_EMPTY=$(ls -A /usr/local/unifing-catalog-${DCP_DOT_VERSION})
 
-  time (
-    wget -q https://storage.googleapis.com/unifi-hd-4tb/unifi-release/${DCP_VERSION}/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz -P /tmp
+echo "unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz"
 
-    sudo tar xvf /tmp/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz -C /tmp > /dev/null 2>&1
+wget -q https://storage.googleapis.com/unifi-hd-4tb/unifi-release/${DCP_VERSION}/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz -P /tmp
+sudo tar xvf /tmp/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz -C /tmp > /dev/null 2>&1
 
-    sudo yum -y install java-1.8.0-openjdk-headless-1.8.0.282.b08 \
-      java-1.8.0-openjdk-1.8.0.282.b08
-  )
+sudo yum -y install java-1.8.0-openjdk-headless-1.8.0.282.b08 \
+  java-1.8.0-openjdk-1.8.0.282.b08
 
-  echo "unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x.tar.gz"
+echo "install_unifi_prereqs_catalog.sh..."
 
-  time (
-    sudo /tmp/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x/install_unifi_prereqs_catalog.sh --unifi-services-user unifi --unifi-services-group unifi
+sudo /tmp/unifi-prereqs-${DCP_VERSION}-catalog-centos-rhel-7.x/install_unifi_prereqs_catalog.sh --unifi-services-user unifi --unifi-services-group unifi
 
-    shopt -s expand_aliases
-    source /home/unifi/.bashrc
-  )
+shopt -s expand_aliases
+source /home/unifi/.bashrc
 
-  echo "Prereq install"
+if [ -z "$IS_EMPTY" ]; 
+then
+  echo "unifing-catalog-${DCP_DOT_VERSION}.tar.gz"
+  wget -q https://storage.googleapis.com/unifi-hd-4tb/unifi-release/${DCP_VERSION}/unifing-catalog-${DCP_DOT_VERSION}.tar.gz -P /tmp
+  sudo tar xvf /tmp/unifing-catalog-${DCP_DOT_VERSION}.tar.gz -C /usr/local > /dev/null 2>&1
+fi
 
-  if [ -z "$IS_EMPTY" ]; 
-  then
-    time (
-      wget -q https://storage.googleapis.com/unifi-hd-4tb/unifi-release/${DCP_VERSION}/unifing-catalog-${DCP_DOT_VERSION}.tar.gz -P /tmp
-      sudo tar xvf /tmp/unifing-catalog-${DCP_DOT_VERSION}.tar.gz -C /usr/local > /dev/null 2>&1
-    )
-    
-    echo "unifing-catalog-${DCP_DOT_VERSION}.tar.gz"
-  fi
+sudo ln -s /usr/local/unifing-catalog-${DCP_DOT_VERSION} /usr/local/unifi
+sudo ln -s /usr/local/unifing-catalog-${DCP_DOT_VERSION}/unifi_pylib/lib/unifi /usr/local/unifi_virtualenv/lib/python3.8/site-packages/
 
-  sudo ln -s /usr/local/unifing-catalog-${DCP_DOT_VERSION} /usr/local/unifi
-  sudo ln -s /usr/local/unifing-catalog-${DCP_DOT_VERSION}/unifi_pylib/lib/unifi /usr/local/unifi_virtualenv/lib/python3.8/site-packages/
-  
-  time (
-    sudo chown -R unifi:unifi /usr/local/unifing-catalog-${DCP_DOT_VERSION}
-    sudo chown -R unifi:unifi /usr/local/unifi_virtualenv
-  
-    echo "UNIFI_VIRT_ENV=/usr/local/unifi_virtualenv" >> /usr/local/unifi/unifi_env.sh
-  )
-  
-  echo "chown unifing-catalog-${DCP_DOT_VERSION}.tar.gz"
+echo "chown /usr/local/unifi"
 
-  pip install /usr/local/unifi/unifi_pylib/django_celery_beat-1.4.0.Unifi-py2.py3-none-any.whl
+sudo chown -R unifi:unifi /usr/local/unifing-catalog-${DCP_DOT_VERSION}
+sudo chown -R unifi:unifi /usr/local/unifi_virtualenv
 
-  time (
-    cd /usr/local/unifi/scripts/sbin
+if [ -z "$IS_EMPTY" ]; 
+then
+  echo "UNIFI_VIRT_ENV=/usr/local/unifi_virtualenv" >> /usr/local/unifi/unifi_env.sh
+fi
 
-    unifi_checkdeps
+pip install /usr/local/unifi/unifi_pylib/django_celery_beat-1.4.0.Unifi-py2.py3-none-any.whl
 
-    if [ -z "$IS_EMPTY" ]; 
-    then
-      sed -i 's/DEFAULT_PEXPECT_TIMEOUT = 600/DEFAULT_PEXPECT_TIMEOUT = 60000/g' unifi_install
-      unifi_install --dbhost 127.0.0.1 --dbport 5432 --dbuser unifi \
-        --dbpass unifi --dbname unifi --chost 127.0.0.1 --cport 9042 \
-        --cuser cassandra --cpass cassandra --unifiuser unifi \
-        --unifipass Password123! --unifiemail noreply@boomi.com \
-        --unififirstname DCP --unifilastname User
-    fi
+cd /usr/local/unifi/scripts/sbin
 
-    unifi_start
-    unifi_status
-  )
+unifi_checkdeps
 
-  echo "unifi_start time"
-)
+if [ -z "$IS_EMPTY" ]; 
+then
+  echo "unifi_install..."
+  sed -i 's/DEFAULT_PEXPECT_TIMEOUT = 600/DEFAULT_PEXPECT_TIMEOUT = 60000/g' unifi_install
+  unifi_install --dbhost 127.0.0.1 --dbport 5432 --dbuser unifi \
+    --dbpass unifi --dbname unifi --chost 127.0.0.1 --cport 9042 \
+    --cuser cassandra --cpass cassandra --unifiuser unifi \
+    --unifipass Password123! --unifiemail noreply@boomi.com \
+    --unififirstname DCP --unifilastname User
+fi
 
-echo "Complete installation"
+echo "unifi_start..."
+
+unifi_start
+unifi_status
 
 sleep infinity
