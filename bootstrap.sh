afac07e671a2d588022bdb4f1d292db350f01ce8
Brian Merrick <brianmerrick@boomi.com>
2022-03-16 16:09:07 -0600

    Adding initial bootstrap script

--- bootstrap.sh
+++ bootstrap.sh
@@ -19,7 +19,7 @@ then
   echo -e "postgres"
   echo -e "dcp"
   echo -e "vault"
-  echo -e "--ALL$GE"
+  echo -e "ALL$GE"
   exit
 fi
 
@@ -27,6 +27,8 @@ echo "Bootstrapping $1..."
 APATH=kubernetes/addons
 
 mysql() {
+  echo -e "$GS-- mysql --$GE"
+
   CHECK="SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = 'demo' AND table_name = 'feature_flags'"
   if [ $(kubectl -n addons-mysql-3306 exec -i mysql-3306-0 -- mysql --batch -u root -ppassword -e "$CHECK" | sed -n '2 p') -eq 0 ];
   then
@@ -43,30 +45,46 @@ mysql() {
 }
 
 oracledb() {
-  # Oracle
-  kubectl -n addons-oracledb-1521 exec -i oracledb-1521-0 -- sqlplus sys/Password123! as sysdba < $APATH/oracledb/datasets/LearningSQL-Oracle-Script.sql
+  echo -e "$GS-- oracledb --$GE"
+
+  CHECK="Select table_Name from user_Tables Where table_name = 'ACCOUNT';"
+  if [ "$(kubectl -n addons-oracledb-1521 exec -i oracledb-1521-0 -- sqlplus -s sys/Password123! as sysdba <<< "$CHECK" | sed -n '2 p')" = "no rows selected" ];
+  then
+    echo -e "$GS -- oracledb -- $GE"
+    kubectl -n addons-oracledb-1521 exec -i oracledb-1521-0 -- sqlplus sys/Password123! as sysdba < $APATH/oracledb/datasets/LearningSQL-Oracle-Script.sql
+  fi
 }
 
 sqlserver() {
-  # SQLServer
-  kubectl -n addons-sqlserver-1433 exec -i sqlserver-1433-0 -- /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Password123! < $APATH/sqlserver/datasets/school-database.sql
+  echo -e "$GS-- sqlserver --$GE"
+
+  CHECK="SELECT COUNT(*) FROM information_schema.tables WHERE table_name = 'Person' AND table_catalog = 'school'"
+  if [ "$(kubectl -n addons-sqlserver-1433 exec -i sqlserver-1433-0 -- /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Password123! -h-1 -W -w 700 -d school -Q "$CHECK" | sed -n '1 p')" != "1" ];
+  then
+    echo -e "$GS -- sqlserver.school -- $GE"
+    kubectl -n addons-sqlserver-1433 exec -i sqlserver-1433-0 -- /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P Password123! < $APATH/sqlserver/datasets/school-database.sql
+  fi
 }
 
 mongodb() {
   # MongoDB
-  kubectl -n addons-mongodb-27017 exec -i mongodb-27017-0 -- mongoimport --db inventory --collection list --authenticationDatabase admin --username root --password password < $APATH/mongodb/datasets/inventory.json
+  #kubectl -n addons-mongodb-27017 exec -i mongodb-27017-0 -- mongoimport --db inventory --collection list --authenticationDatabase admin --username root --password password < $APATH/mongodb/datasets/inventory.json
+  echo -e "$GS-- mongodb --$GE"
 }
 
 postgres() {
   # Postgres
-  kubectl -n addons-postgres-5432 exec -i postgres-5432-0 -- pg_restore --dbname=postgresql://postgres:password@localhost:5432/demo < $APATH/postgres/datasets/dvdrental.tar
+  #kubectl -n addons-postgres-5432 exec -i postgres-5432-0 -- pg_restore --dbname=postgresql://postgres:password@localhost:5432/demo < $APATH/postgres/datasets/dvdrental.tar
+  echo -e "$GS-- postgres --$GE"
 }
 
 vault() {
-  echo "vault"
+  echo -e "$GS-- vault --$GE"
 }
 
 dcp() {
+  echo -e "$GS-- dcp --$GE"
+
   if [ -z "$1" ];
   then
     echo -e "\033[0;31mDCP password is required\033[0m"
@@ -155,26 +173,31 @@ then
   mysql
 elif [ "$1" = "oracledb" ];
 then
-  echo "oracledb"
+  oracledb
 elif [ "$1" = "sqlserver" ];
 then
-  echo "sqlserver"
+  sqlserver
 elif [ "$1" = "mongodb" ];
 then
-  echo "mongodb"
+  mongodb
 elif [ "$1" = "postgres" ];
 then
-  echo "postgres"
+  postgres
 elif [ "$1" = "vault" ];
 then
-  echo "vault"
+  vault
 elif [ "$1" = "dcp" ];
 then
   echo "DCP is not ready, exiting..."
   exit
   dcp $2
-elif [ "$1" = "--ALL" ];
+elif [ "$1" = "ALL" ];
 then
-  echo "--ALL"
+  mysql
+  oracledb
+  sqlserver
+  mongodb
+  postgres
+  vault
 fi
 
